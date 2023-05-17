bf0e8c2c67dc9fbb2e4a3bef048c35c79e31a4a5
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-18 00:40:41 +0200

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Weather from "./Weather.js";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./Weather.css";
 
@@ -10,7 +11,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   </StrictMode>
 );
