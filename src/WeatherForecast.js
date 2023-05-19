0e877f430629d625633a83274ad11ef75c60cfd3
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-19 15:55:02 +0200

    refactored code and fixed warnings

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -7,23 +7,27 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
-  if (loaded) {
+  function load() {
     let apiKey = "8c78e9e7e9928cd1a2a6f923072c3dec";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-
+  }
+  if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-            
           {forecast.map(function (dailyForecast, index) {
             if (index < 6) {
               return (
@@ -31,12 +35,16 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
+    load();
+
     return null;
   }
 }
